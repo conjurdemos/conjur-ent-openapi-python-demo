756ae8ee2c2af1c174cee160b26c6d1dcafa6c15
Joseph Hunt <jodyhunt@hotmail.com>
2022-03-31 15:43:29 -0500

    cleanup

--- 0-config.sh
+++ 0-config.sh
@@ -1,11 +1,11 @@
-# Conjur Open API spec repo directory
+# path to Conjur Open API spec repo directory
 #   see: https://github.com/cyberark/conjur-openapi-spec
 GEN_DIR=../conjur-openapi-spec-main
 
 # Conjur service configuration variables
-CONJUR_ACCOUNT=dev
-CONJUR_APPLIANCE_HOSTNAME=conjur-master-minikube
-CONJUR_HOST_IP_ADDRESS=192.168.59.100
-CONJUR_CERT=~/Conjur/cybrsm-demos/etc/conjur-master-minikube-dev.pem
-CONJUR_AUTHN_LOGIN=admin
-CONJUR_AUTHN_API_KEY=$(keyring get conjur adminpwd)
+CONJUR_ACCOUNT=<conjur-account>
+CONJUR_APPLIANCE_HOSTNAME=<conjur-hostname>
+CONJUR_HOST_IP_ADDRESS=<conjur-ip-address>
+CONJUR_CERT=<path-to-conjur-cert-file>
+CONJUR_AUTHN_LOGIN=<admin-username>
+CONJUR_AUTHN_API_KEY=<admin-password>
